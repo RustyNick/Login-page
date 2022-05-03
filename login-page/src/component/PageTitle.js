920ab14e727fb26c4f5033658f60eb3e099f1994
Niklas <niklas.hadell@outlook.com>
2022-05-03 17:20:25 +0200

    Margin, padding and width fix of Input and Checkmark

---
+++ login-page/src/component/PageTitle.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function PageTitle() {
+  return (
+    <div>
+      <h3>
+        Welcome!
+        <br/>
+        Create your account.
+      </h3>
+<h5>Already a member? <a href="#">Sign up</a> </h5>
+        
+    </div>
+  )
+}
+
+export default PageTitle
--- login-page/src/component/RightContainer.js
+++ login-page/src/component/RightContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Form from './Form'
+import PageTitle from './PageTitle'
 
 function RightContainer() {
   return (
     <div className='container container__right'>
+        <PageTitle/>
         <Form/>
     </div>
   )
